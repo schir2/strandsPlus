42edc2172377dbf036ead95819ab0496b35a1118
Marek Schir <schir2@msn.com>
2024-06-25 00:59:59 -0400

    Update PuzzleState.cs
    added additional states needed for scoring

--- Assets/Scripts/PuzzleState.cs
+++ Assets/Scripts/PuzzleState.cs
@@ -2,15 +2,22 @@ using System.Collections.Generic;
 
 public class PuzzleState
 {
-    public HashSet<string> validWordsGuessed = new HashSet<string>();
     public int hints = 0;
-    public int guessCount = 0;
+    public int hintsUsed = 0;
+    public int guessesUsed = 0;
+    public int currentStreak = 0;
+    public int longestStreak = 0;
     public int correctGuessCount = 0;
-    public HashSet<string> correctWordsGuessed = new HashSet<string>();
-    public HashSet<string> wordsGuessed = new HashSet<string>();
+    public int elapsedTimeInSeconds = 0;
     public bool spangramFound = false;
     public string lastWordGuessed;
+    public bool solved = false;
+
     public Puzzle.GuessResult lastGuessResult;
+
+    public HashSet<string> validWordsGuessed = new HashSet<string>();
+    public HashSet<string> correctWordsGuessed = new HashSet<string>();
+    public HashSet<string> wordsGuessed = new HashSet<string>();
 
     public void IncrementCorrectGuessCount()
     {
