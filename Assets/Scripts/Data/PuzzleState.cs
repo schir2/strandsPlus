d9100f7d93b6d7a02cd71466f22ae4fb3c57eef6
Marek Schir <schir2@gmail.com>
2024-06-29 22:13:39 -0400

    Refactor PuzzleState and add hint usage logic
    The PuzzleState's member variables have been refactored for consistent naming, and unused variables have been removed. Additionally, logic has been added to allow the use of hints, adjusting the hint count and tracking usage appropriately.

--- Assets/Scripts/Data/PuzzleState.cs
+++ Assets/Scripts/Data/PuzzleState.cs
@@ -6,29 +6,37 @@ namespace Data
 {
     public class PuzzleState
     {
-        public HashSet<string> validWordsGuessed = new();
         public int Hints = 0;
         public int GuessCount = 0;
         private int correctGuessCount = 0;
-        public HashSet<string> correctWordsGuessed = new();
-        public HashSet<string> wordsGuessed = new();
+        public readonly HashSet<string> CorrectWordsGuessed = new();
+        public readonly HashSet<string> WordsGuessed = new();
         public bool spangramFound = false;
         public string lastWordGuessed;
-        public Puzzle.GuessResult lastGuessResult;
+        public Puzzle.GuessResult LastGuessResult;
         public int hintsUsedCount = 0;
         public int longestStreak = 0;
         public int currentStreak = 0;
+        public string revealedWord = null;
         public int spangramFoundIn = int.MaxValue;
 
         public int PuzzleWordsFound()
         {
-            return correctWordsGuessed.Count + (spangramFound ? 1 : 0);
+            return CorrectWordsGuessed.Count + (spangramFound ? 1 : 0);
+        }
+
+        public bool UseHint()
+        {
+            if (revealedWord != null || Hints <= 0) return false;
+            hintsUsedCount++;
+            Hints--;
+            return true;
         }
 
         public void UpdateState(Puzzle.GuessResult guessResult, string word)
         {
             lastWordGuessed = word;
-            lastGuessResult = guessResult;
+            LastGuessResult = guessResult;
 
             switch (guessResult)
             {
@@ -42,8 +50,13 @@ namespace Data
                 case Puzzle.GuessResult.Correct:
                     Debug.Log($"Correct word found {word}");
                     GuessCount++;
-                    correctWordsGuessed.Add(word);
+                    CorrectWordsGuessed.Add(word);
                     currentStreak++;
+                    if (word == revealedWord)
+                    {
+                        revealedWord = null;
+                    }
+
                     break;
                 case Puzzle.GuessResult.Valid:
                     Debug.Log($"Valid found {word}");
@@ -59,7 +72,7 @@ namespace Data
             }
 
             longestStreak = Math.Max(longestStreak, currentStreak);
-            wordsGuessed.Add(word);
+            WordsGuessed.Add(word);
         }
     }
 }
