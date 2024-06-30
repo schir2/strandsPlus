a4141cd0be627079a07e7f2d7d95f1a3adae07ad
Marek Schir <schir2@gmail.com>
2024-06-29 20:55:50 -0400

    Refactor PuzzleState and update guessing logic
    In this update, the 'PuzzleState' script was refactored, mainly with modified variable declarations. The guessing logic within 'UpdateState' method was also updated to handle different results and manage game stats accordingly. This includes tracking game state variables like the current streak, longest streak, guess count and hints.

--- Assets/Scripts/Data/PuzzleState.cs
+++ Assets/Scripts/Data/PuzzleState.cs
@@ -1,15 +1,17 @@
+using System;
 using System.Collections.Generic;
+using UnityEngine;
 
 namespace Data
 {
     public class PuzzleState
     {
-        public HashSet<string> validWordsGuessed = new HashSet<string>();
-        public int hints = 0;
-        public int guessCount = 0;
-        public int correctGuessCount = 0;
-        public HashSet<string> correctWordsGuessed = new HashSet<string>();
-        public HashSet<string> wordsGuessed = new HashSet<string>();
+        public HashSet<string> validWordsGuessed = new();
+        public int Hints = 0;
+        public int GuessCount = 0;
+        private int correctGuessCount = 0;
+        public HashSet<string> correctWordsGuessed = new();
+        public HashSet<string> wordsGuessed = new();
         public bool spangramFound = false;
         public string lastWordGuessed;
         public Puzzle.GuessResult lastGuessResult;
@@ -18,14 +20,46 @@ namespace Data
         public int currentStreak = 0;
         public int spangramFoundIn = int.MaxValue;
 
-        public void IncrementCorrectGuessCount()
-        {
-            correctGuessCount++;
-        }
-
         public int PuzzleWordsFound()
         {
             return correctWordsGuessed.Count + (spangramFound ? 1 : 0);
+        }
+
+        public void UpdateState(Puzzle.GuessResult guessResult, string word)
+        {
+            lastWordGuessed = word;
+            lastGuessResult = guessResult;
+
+            switch (guessResult)
+            {
+                case Puzzle.GuessResult.Spangram:
+                    Debug.Log($"Spangram found {word}");
+                    GuessCount++;
+                    spangramFound = true;
+                    spangramFoundIn = GuessCount;
+                    currentStreak++;
+                    break;
+                case Puzzle.GuessResult.Correct:
+                    Debug.Log($"Correct word found {word}");
+                    GuessCount++;
+                    correctWordsGuessed.Add(word);
+                    currentStreak++;
+                    break;
+                case Puzzle.GuessResult.Valid:
+                    Debug.Log($"Valid found {word}");
+                    GuessCount++;
+                    correctGuessCount++;
+                    currentStreak++;
+                    Hints += (correctGuessCount % 3 == 0 ? 1 : 0);
+                    break;
+                case Puzzle.GuessResult.Invalid:
+                    Debug.Log($"Invalid word found {word}");
+                    currentStreak = 0;
+                    break;
+            }
+
+            longestStreak = Math.Max(longestStreak, currentStreak);
+            wordsGuessed.Add(word);
         }
     }
 }
